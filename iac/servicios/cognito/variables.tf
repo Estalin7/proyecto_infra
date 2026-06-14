84aed6eaee7dc96a6677210173979f4c09a287ea
bryang1205 <bryantulumba@gmail.com>
2026-06-13 20:34:10 -0500

    feat: codificacion de archivos tf en subcarpeta cognito

---
+++ iac/servicios/cognito/variables.tf
@@ -0,0 +1,9 @@
+variable "project" {
+  description = "Nombre del proyecto"
+  type        = string
+}
+
+variable "environment" {
+  description = "Entorno de despliegue"
+  type        = string
+}
