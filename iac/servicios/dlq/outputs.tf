cf91fde5c3ad4457f732296fc05380db82755e65
bryang1205 <bryantulumba@gmail.com>
2026-06-13 20:36:49 -0500

    feat: codificacion de archivos tf en subcarpeta dlq

--- iac/servicios/dlq/outputs.tf
+++ iac/servicios/dlq/outputs.tf
@@ -1,1 +1,10 @@
 # Outputs del módulo DLQ
+output "dlq_arn" {
+  description = "ARN de la DLQ (se pasa al modulo sqs para la redrive policy)"
+  value       = aws_sqs_queue.dlq.arn
+}
+
+output "dlq_url" {
+  description = "URL de la DLQ"
+  value       = aws_sqs_queue.dlq.url
+}
