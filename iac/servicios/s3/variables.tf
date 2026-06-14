72b719b57f94b11491e851cad0375d07aeb6b1da
bryang1205 <bryantulumba@gmail.com>
2026-06-13 20:59:34 -0500

    feat: codificacion de archivos tf en subcarpeta s3

---
+++ iac/servicios/s3/variables.tf
@@ -0,0 +1,14 @@
+variable "project" {
+  description = "Nombre del proyecto"
+  type        = string
+}
+
+variable "environment" {
+  description = "Entorno de despliegue"
+  type        = string
+}
+
+variable "cloudfront_distribution_arn" {
+  description = "ARN de la distribucion CloudFront (para la politica OAC del bucket frontend)"
+  type        = string
+}
