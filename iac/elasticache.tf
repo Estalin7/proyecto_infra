137e75683a34a76e7df6d0fcc48d4fb531ba3d17
bryang1205 <bryantulumba@gmail.com>
2026-06-26 15:16:42 -0500

    feat: creacion de pipile de sonarqube en rama hotfix

--- iac/elasticache.tf
+++ iac/elasticache.tf
@@ -15,39 +15,6 @@ resource "aws_elasticache_subnet_group" "main" {
   }
 }
 
-resource "aws_kms_key" "elasticache" {
-  description             = "KMS key para ElastiCache Redis ${var.project}-${var.environment}"
-  deletion_window_in_days = 7
-  enable_key_rotation     = true
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Sid    = "EnableRootAccess"
-        Effect = "Allow"
-        Principal = {
-          AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"
-        }
-        Action   = "kms:*"
-        Resource = "*"
-      },
-      {
-        Sid    = "AllowElastiCacheUse"
-        Effect = "Allow"
-        Principal = { Service = "elasticache.amazonaws.com" }
-        Action   = ["kms:Decrypt", "kms:GenerateDataKey"]
-        Resource = "*"
-      }
-    ]
-  })
-
-  tags = {
-    Name        = "${var.project}-kms-redis-${var.environment}"
-    Project     = var.project
-    Environment = var.environment
-  }
-}
 
 resource "aws_elasticache_replication_group" "main" {
   replication_group_id = "${var.project}-redis-${var.environment}"
@@ -63,7 +30,6 @@ resource "aws_elasticache_replication_group" "main" {
   security_group_ids = [aws_security_group.elasticache.id]
 
   at_rest_encryption_enabled = true
-  kms_key_id                 = aws_kms_key.elasticache.arn
   transit_encryption_enabled = true
   auth_token                 = var.redis_auth_token
 
